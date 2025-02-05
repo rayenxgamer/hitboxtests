6f156398a633331af9d3f16cfdf4785cde728134
rayengamer <rayenxgamer@gmail.com>
2025-02-05 15:42:49 +0000

    hello world!

---
+++ build/CMakeFiles/hitboxtests.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hitboxtests.dir/link.d"
+  "CMakeFiles/hitboxtests.dir/src/glad.c.o"
+  "CMakeFiles/hitboxtests.dir/src/glad.c.o.d"
+  "CMakeFiles/hitboxtests.dir/src/main.c.o"
+  "CMakeFiles/hitboxtests.dir/src/main.c.o.d"
+  "CMakeFiles/hitboxtests.dir/src/stbimage.c.o"
+  "CMakeFiles/hitboxtests.dir/src/stbimage.c.o.d"
+  "hitboxtests"
+  "hitboxtests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hitboxtests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/hitboxtests.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hitboxtests.
+# This may be replaced when dependencies are built.
